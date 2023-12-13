1fd01d291ba321aca01d1d7eb39b77ff04fe8a9e
Massimo Nocentini <massimo.nocentini@gmail.com>
2023-12-13 17:37:06 +0100

    Two small refinments to compile on a fresh box.

--- scripts/cygwin-build.sh
+++ scripts/cygwin-build.sh
@@ -2,6 +2,10 @@
 set -e
 
 cd ../ 
+
+cp -r /cygdrive/c/msys64/usr/local/include /usr/local
+cp -r /cygdrive/c/msys64/usr/local/lib /usr/local
+
 cmake --build pharo-vm-build --target install
 rm -rf pharo-vm-dist
 cp -r pharo-vm-build/build/dist pharo-vm-dist
--- scripts/cygwin-prepareCompilation.sh
+++ scripts/cygwin-prepareCompilation.sh
@@ -10,4 +10,3 @@ wget http://files.pharo.org/vm/pharo-spur64-headless/Windows-x86_64/source/Pharo
 unzip PharoVM-10.0.9-de760673-Windows-x86_64-c-src.zip
 cd ../
 cmake -S pharo-vm -B pharo-vm-build -DPHARO_DEPENDENCIES_PREFER_DOWNLOAD_BINARIES=TRUE -DBUILD_IS_RELEASE=ON -DICEBERG_DEFAULT_REMOTE=httpsUrl -DGENERATE_SOURCES=FALSE -DGENERATED_SOURCE_DIR=../pharo-vm-c-src/pharo-vm/
-			
