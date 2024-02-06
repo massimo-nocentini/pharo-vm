8eb8d6dcd6cc5bb3e510b79d3d5ed1ed5ab397e6
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-02-06 10:00:44 +0100

    Update to the new pharo-10 branch

--- scripts/cygwin-prepareCompilation.sh
+++ scripts/cygwin-prepareCompilation.sh
@@ -25,8 +25,8 @@ cd ../pharo-vm-compiling
 rm -rf pharo-vm-c-src
 mkdir -p pharo-vm-c-src
 cd pharo-vm-c-src
-wget http://files.pharo.org/vm/pharo-spur64-headless/Windows-x86_64/source/PharoVM-10.0.9-de760673-Windows-x86_64-c-src.zip
-unzip PharoVM-10.0.9-de760673-Windows-x86_64-c-src.zip
+wget https://files.pharo.org/vm/pharo-spur64-headless/Windows-x86_64/source/PharoVM-10.0.9-79fe4f32-Windows-x86_64-c-src.zip
+unzip PharoVM-10.0.9-79fe4f32-Windows-x86_64-c-src.zip
 cd ../
 
 cmake -S pharo-vm -B pharo-vm-build -DUSE_MSYS2=TRUE -DPHARO_DEPENDENCIES_PREFER_DOWNLOAD_BINARIES=TRUE -DBUILD_IS_RELEASE=ON -DICEBERG_DEFAULT_REMOTE=httpsUrl -DGENERATE_SOURCES=FALSE -DGENERATED_SOURCE_DIR=../pharo-vm-c-src/pharo-vm/
