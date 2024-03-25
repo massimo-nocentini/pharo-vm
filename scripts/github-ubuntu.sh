a825877c98ec5509b12ed0f1a051f3d1834c8f3e
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-03-25 11:09:29 +0100

    Cleanup and copy some shared objects.

--- scripts/github-ubuntu.sh
+++ scripts/github-ubuntu.sh
@@ -22,10 +22,12 @@ wget https://files.pharo.org/vm/pharo-spur64-headless/Linux-x86_64/source/PharoV
 unzip PharoVM-10.1.1-32b2be5-Linux-x86_64-c-src.zip
 cd ..
 
-#export CC=clang && export CXX=clang++
-cmake -S pharo-vm -B pharo-vm-build -DPHARO_DEPENDENCIES_PREFER_DOWNLOAD_BINARIES=TRUE -DBUILD_IS_RELEASE=ON -DICEBERG_DEFAULT_REMOTE=httpsUrl -DGENERATE_SOURCES=TRUE #-DGENERATED_SOURCE_DIR=../pharo-vm-c-src/pharo-vm/
+cmake -S pharo-vm -B pharo-vm-build -DPHARO_DEPENDENCIES_PREFER_DOWNLOAD_BINARIES=TRUE -DBUILD_IS_RELEASE=ON -DICEBERG_DEFAULT_REMOTE=httpsUrl -DGENERATE_SOURCES=FALSE -DGENERATED_SOURCE_DIR=../pharo-vm-c-src/pharo-vm/
 cmake --build pharo-vm-build --target install
 rm -rf build/ pharo-vm-build/build/dist/lib/{libss*,libcairo.so*,libgit2.*,libharfbuzz.so*,libfontconfig.so*} #,libbz2*,libexpat*,libffi*,libfreetype*,libpixman*,libpng*"
+
+cp /usr/lib/x86_64-linux-gnu/{libssh2.so,libssl.so,libcairo-gobject.so,libcairo.so,libpango-1.0.so,libpangocairo-1.0.so,libgit2-glib-1.0.so,libgit2.so,libharfbuzz-cairo.so,libharfbuzz-gobject.so,libharfbuzz-icu.so,libharfbuzz.so,libharfbuzz-subset.so,libfontconfig.so} /usr/local/lib/liblua.a pharo-vm-build/build/dist/lib/
+
 
 cd pharo-vm-build/build/dist/
 zip -r pharo-vm-ubuntu.zip *
