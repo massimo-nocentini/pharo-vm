5482e50630c337ae5c128f106c62b21c27b91ff9
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-04-22 10:28:18 +0200

    Split the upload of different Pharo images.

--- scripts/github-fetch-images.sh
+++ scripts/github-fetch-images.sh
@@ -5,31 +5,34 @@ cd fresh-images
 mkdir 90
 cd 90
 curl https://get.pharo.org/64/90 | bash
+zip -r Pharo64-90.zip *
 cd ..
 
 mkdir 100
 cd 100
 curl https://get.pharo.org/64/100 | bash
+zip -r Pharo64-100.zip *
 cd ..
 
 
 mkdir 110
 cd 110
 curl https://get.pharo.org/64/110 | bash
+zip -r Pharo64-110.zip *
 cd ..
 
 
 mkdir 120
 cd 120
 curl https://get.pharo.org/64/120 | bash
+zip -r Pharo64-120.zip *
 cd ..
 
 
 mkdir alpha
 cd alpha
 curl https://get.pharo.org/64/alpha | bash
+zip -r Pharo64-alpha.zip *
 cd ..
-
-zip -r fresh-images.zip *
 
 cd ..
