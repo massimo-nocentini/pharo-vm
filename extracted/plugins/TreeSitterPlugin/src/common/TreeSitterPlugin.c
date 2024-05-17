a1a362b72cdb50c09032bcd98a892f1508026468
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-05-17 09:15:20 +0200

    Implementation of the ast function for the TreeSitterPlugin.

--- extracted/plugins/TreeSitterPlugin/src/common/TreeSitterPlugin.c
+++ extracted/plugins/TreeSitterPlugin/src/common/TreeSitterPlugin.c
@@ -91,18 +91,9 @@ char *checked_cStringOrNullFor(sqInt oop, int *should_free)
 	return *should_free ? interpreterProxy->cStringOrNullFor(oop) : str;
 }
 
-/*	Note: This is coded so that it can be run in Squeak. */
-
-/* InterpreterPlugin>>#setInterpreter: */
-EXPORT(sqInt)
-setInterpreter(struct VirtualMachine *anInterpreter)
+sqInt checked_stringForCString(const char *cString)
 {
-	sqInt ok;
-
-	interpreterProxy = anInterpreter;
-	ok = ((interpreterProxy->majorVersion()) == (VM_PROXY_MAJOR)) && ((interpreterProxy->minorVersion()) >= (VM_PROXY_MINOR));
-
-	return ok;
+	return interpreterProxy->stringForCString(cString == NULL ? "" : cString);
 }
 
 TSLanguage *tree_sitter_c();
@@ -125,45 +116,188 @@ primitive_tree_sitter_c(void)
 	return null;
 }
 
-/*
+EXPORT(sqInt)
+primitive_ts_parser_new(void)
+{
 
+	TSParser *parser = ts_parser_new();
 
-	lua_newtable(L);
+	sqInt anExternalAddress = newExternalAddress();
 
-	lua_pushlightuserdata(L, lang);
-	lua_setfield(L, -2, "language_handler");
+	writeAddress(anExternalAddress, parser);
 
-	FILE *fptr;
-
-	fptr = fopen(HIGHLIGHTS_C_FILEPATH, "r");
-
-	if (fptr != NULL)
+	if (!(interpreterProxy->failed()))
 	{
-
-		char c;
-
-		luaL_Buffer b;
-		luaL_buffinit(L, &b);
-
-		while ((c = fgetc(fptr)) != EOF)
-		{
-			luaL_addchar(&b, c);
-		}
-
-		luaL_pushresult(&b);
-	}
-	else
-	{
-		lua_pushstring(L, "");
+		interpreterProxy->popthenPush(1, anExternalAddress);
 	}
 
-	fclose(fptr);
+	return null;
+}
 
-	lua_setfield(L, -2, "query_highlights");
+EXPORT(sqInt)
+primitive_ts_parser_delete(void)
+{
 
-	lua_setfield(L, -2, "c");
+	TSParser *parser = readAddress(interpreterProxy->stackValue(0));
 
-*/
+	ts_parser_delete(parser);
+
+	if (!(interpreterProxy->failed()))
+	{
+		interpreterProxy->pop(1); // just leave the receiver on the stack
+	}
+
+	return null;
+}
+
+EXPORT(sqInt)
+primitive_ts_parser_set_language(void)
+{
+	TSParser *parser = readAddress(interpreterProxy->stackValue(1));
+	TSLanguage *language = readAddress(interpreterProxy->stackValue(0));
+
+	bool assigned = ts_parser_set_language(parser, language);
+
+	if (!(interpreterProxy->failed()))
+	{
+		interpreterProxy->pop(3);
+		interpreterProxy->pushBool(assigned);
+	}
+
+	return null;
+}
+
+EXPORT(sqInt)
+primitive_ts_parser_parse_string(void)
+{
+
+	TSParser *parser = readAddress(interpreterProxy->stackValue(1));
+	char *source_code = interpreterProxy->firstIndexableField(interpreterProxy->stackValue(0));
+	sqInt length = interpreterProxy->stSizeOf(interpreterProxy->stackValue(0));
+
+	TSTree *tree = ts_parser_parse_string(
+		parser,
+		NULL,
+		source_code,
+		length);
+
+	sqInt anExternalAddress = newExternalAddress();
+
+	writeAddress(anExternalAddress, tree);
+
+	if (!(interpreterProxy->failed()))
+	{
+		interpreterProxy->popthenPush(3, anExternalAddress);
+	}
+
+	return null;
+}
+
+EXPORT(sqInt)
+primitive_ts_tree_delete(void)
+{
+
+	TSTree *tree = readAddress(interpreterProxy->stackValue(0));
+
+	ts_tree_delete(tree);
+
+	if (!(interpreterProxy->failed()))
+	{
+		interpreterProxy->pop(1); // just leave the receiver on the stack
+	}
+
+	return null;
+}
+
+sqInt walk(sqInt class, sqInt cumulatedArray, const char *src, const TSLanguage *lang, TSNode node, int node_pos, const char *field_name)
+{
+	int fieldIndex = 0;
+
+	sqInt oop = interpreterProxy->instantiateClassindexableSize(class, 0);
+
+	const char *type = ts_node_type(node);
+	TSSymbol symbol_id = ts_node_symbol(node);
+	TSPoint start_point = ts_node_start_point(node);
+	TSPoint end_point = ts_node_end_point(node);
+	const char *symbol_name = ts_language_symbol_name(lang, symbol_id);
+	int start = interpreterProxy->integerValueOf(interpreterProxy->stObjectat(cumulatedArray, start_point.row + 1)) + start_point.column;
+	int end = interpreterProxy->integerValueOf(interpreterProxy->stObjectat(cumulatedArray, end_point.row + 1)) + end_point.column;
+	int n = end - start;
+	char *token = (char *)malloc(sizeof(char) * n + 1);
+	strncpy(token, src + start, n);
+	token[n] = '\0';
+
+	interpreterProxy->storePointerofObjectwithValue(fieldIndex++, oop, checked_stringForCString(field_name));
+	interpreterProxy->storeIntegerofObjectwithValue(fieldIndex++, oop, node_pos);
+	interpreterProxy->storePointerofObjectwithValue(fieldIndex++, oop, checked_stringForCString(type));
+	interpreterProxy->storeIntegerofObjectwithValue(fieldIndex++, oop, symbol_id);
+	interpreterProxy->storePointerofObjectwithValue(fieldIndex++, oop, checked_stringForCString(symbol_name));
+	interpreterProxy->storePointerofObjectwithValue(fieldIndex++, oop, interpreterProxy->makePointwithxValueyValue(start, end));
+	interpreterProxy->storePointerofObjectwithValue(fieldIndex++, oop, checked_stringForCString(token));
+	interpreterProxy->storePointerofObjectwithValue(fieldIndex++, oop, interpreterProxy->makePointwithxValueyValue(start_point.row, start_point.column));
+	interpreterProxy->storePointerofObjectwithValue(fieldIndex++, oop, interpreterProxy->makePointwithxValueyValue(end_point.row, end_point.column));
+	interpreterProxy->storePointerofObjectwithValue(fieldIndex++, oop, ts_node_is_null(node) ? interpreterProxy->trueObject() : interpreterProxy->falseObject());
+	interpreterProxy->storePointerofObjectwithValue(fieldIndex++, oop, ts_node_is_named(node) ? interpreterProxy->trueObject() : interpreterProxy->falseObject());
+	interpreterProxy->storePointerofObjectwithValue(fieldIndex++, oop, ts_node_is_missing(node) ? interpreterProxy->trueObject() : interpreterProxy->falseObject());
+	interpreterProxy->storePointerofObjectwithValue(fieldIndex++, oop, ts_node_is_extra(node) ? interpreterProxy->trueObject() : interpreterProxy->falseObject());
+	interpreterProxy->storePointerofObjectwithValue(fieldIndex++, oop, ts_node_has_error(node) ? interpreterProxy->trueObject() : interpreterProxy->falseObject());
+
+	free(token);
+
+	uint32_t c = ts_node_child_count(node);
+
+	sqInt oopChildren = interpreterProxy->instantiateClassindexableSize(interpreterProxy->classArray(), c);
+
+	TSNode child;
+	for (int i = 0; i < c; i++)
+	{
+		child = ts_node_child(node, i);
+
+		interpreterProxy->stObjectatput(oopChildren,
+										i + 1,
+										walk(class, cumulatedArray, src, lang, child, i + 1, ts_node_field_name_for_child(node, i)));
+	}
+
+	interpreterProxy->storePointerofObjectwithValue(fieldIndex++, oop, oopChildren);
+
+	return oop;
+}
+
+EXPORT(sqInt)
+primitive_ts_ast(void)
+{
+
+	TSTree *tree = readAddress(interpreterProxy->stackValue(3));
+	char *src = interpreterProxy->firstIndexableField(interpreterProxy->stackValue(2));
+	sqInt cumulatedSizes = interpreterProxy->stackValue(1);
+	sqInt class = interpreterProxy->stackValue(0);
+
+	TSNode root_node = ts_tree_root_node(tree);
+	const TSLanguage *lang = ts_tree_language(tree);
+
+	sqInt w = walk(class, cumulatedSizes, src, lang, root_node, 1, NULL);
+
+	if (!(interpreterProxy->failed()))
+	{
+		interpreterProxy->popthenPush(5, w);
+	}
+
+	return null;
+}
+
+/*	Note: This is coded so that it can be run in Squeak. */
+
+/* InterpreterPlugin>>#setInterpreter: */
+EXPORT(sqInt)
+setInterpreter(struct VirtualMachine *anInterpreter)
+{
+	sqInt ok;
+
+	interpreterProxy = anInterpreter;
+	ok = ((interpreterProxy->majorVersion()) == (VM_PROXY_MAJOR)) && ((interpreterProxy->minorVersion()) >= (VM_PROXY_MINOR));
+
+	return ok;
+}
 
 /* SmartSyntaxInterpreterPlugin>>#sqAssert: */
 static sqInt
