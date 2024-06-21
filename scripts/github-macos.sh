e10946300b865a3c24e3c1fba14bccbd04981c19
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-06-21 10:57:52 +0200

    Explicitly reference cmake in Github actions for macos.

--- scripts/github-macos.sh
+++ scripts/github-macos.sh
@@ -50,8 +50,12 @@ wget https://files.pharo.org/vm/pharo-spur64-headless/Darwin-x86_64/source/Pharo
 unzip PharoVM-10.2.1-d1bfe9ec-Darwin-x86_64-c-src.zip
 cd ..
 
-cmake -S pharo-vm -B pharo-vm-build -DALWAYS_INTERACTIVE=TRUE -DPHARO_DEPENDENCIES_PREFER_DOWNLOAD_BINARIES=TRUE -DBUILD_IS_RELEASE=ON -DICEBERG_DEFAULT_REMOTE=httpsUrl -DGENERATE_SOURCES=TRUE #-DGENERATED_SOURCE_DIR=../pharo-vm-c-src/pharo-vm/
-cmake --build pharo-vm-build --target install
+pwd
+
+ls
+
+/usr/local/Cellar/cmake/3.29.6/bin/cmake -S pharo-vm -B pharo-vm-build -DALWAYS_INTERACTIVE=TRUE -DPHARO_DEPENDENCIES_PREFER_DOWNLOAD_BINARIES=TRUE -DBUILD_IS_RELEASE=ON -DICEBERG_DEFAULT_REMOTE=httpsUrl -DGENERATE_SOURCES=TRUE #-DGENERATED_SOURCE_DIR=../pharo-vm-c-src/pharo-vm/
+/usr/local/Cellar/cmake/3.29.6/bin/cmake --build pharo-vm-build --target install
 #rm -rf build/ pharo-vm-build/build/dist/lib/{libss*,libcairo.so*,libgit2.*,libharfbuzz.so*,libfontconfig.so*} #,libbz2*,libexpat*,libffi*,libfreetype*,libpixman*,libpng*"
 
 #cp /usr/lib/x86_64-linux-gnu/{libssh2.so,libssl.so,libcairo-gobject.so,libcairo.so,libpango-1.0.so,libpangocairo-1.0.so,libgit2-glib-1.0.so,libgit2.so,libharfbuzz-cairo.so,libharfbuzz-gobject.so,libharfbuzz-icu.so,libharfbuzz.so,libharfbuzz-subset.so,libfontconfig.so} pharo-vm-build/build/dist/lib/
